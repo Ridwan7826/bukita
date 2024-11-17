53f8f56faaf3deab65a5827e2c03861fb43e0b0a
Ridwan7826 <134142931+Ridwan7826@users.noreply.github.com>
2024-11-17 13:15:04 +0800

    Initial commit

---
+++ app/views/templates/Footer.php
@@ -0,0 +1,19 @@
+ <footer class="main-footer">
+     <b>Bukita</b> All rights reserved.
+ </footer>
+
+
+ </div>
+ <!-- ./wrapper -->
+
+ <!-- jQuery -->
+ <script src="<?= base_url; ?>/public/plugins/jquery/jquery.min.js"></script>
+ <!-- Bootstrap 4 -->
+ <script src="<?= base_url; ?>/public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+ <!-- AdminLTE App -->
+ <script src="<?= base_url; ?>/public/dist/js/adminlte.min.js"></script>
+ <!-- AdminLTE for demo purposes -->
+ <script src="<?= base_url; ?>/public/dist/js/demo.js"></script>
+ </body>
+
+ </html>
